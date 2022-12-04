96bb50f10c62865f01c62a49b94345527e3d5503
Raphael Tarita <raphael.tarita@gmail.com>
2022-12-04 12:00:20 +0100

    [DAY 3] solution

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-![](https://img.shields.io/badge/day%20📅-2-yellow)
-![](https://img.shields.io/badge/stars%20⭐-4-blue)
-![](https://img.shields.io/badge/days%20completed-2-green)
+![](https://img.shields.io/badge/day%20📅-4-yellow)
+![](https://img.shields.io/badge/stars%20⭐-6-blue)
+![](https://img.shields.io/badge/days%20completed-3-green)
 
 # AoC-2022
 
--- src/main/kotlin/com/rtarita/days/Day3.kt
+++ src/main/kotlin/com/rtarita/days/Day3.kt
@@ -5,13 +5,33 @@ import com.rtarita.util.day
 import kotlinx.datetime.LocalDate
 
 object Day3 : AoCDay {
+    private const val LOWER_A = 'a'
+    private const val UPPER_A = 'A'
     override val day: LocalDate = day(3)
 
-    override fun executePart1(input: String): Any {
-        return "(not implemented)"
+    private fun splitInHalf(str: String): Pair<Set<Char>, Set<Char>> {
+        return str.substring(0, str.length / 2).toSet() to str.substring(str.length / 2).toSet()
     }
 
-    override fun executePart2(input: String): Any {
-        return "(not implemented)"
+    private fun Char.priority(): Int {
+        return when {
+            isLowerCase() -> code - LOWER_A.code + 1
+            isUpperCase() -> code - UPPER_A.code + 27
+            else -> error("invalid character: $this")
+        }
+    }
+
+    override fun executePart1(input: String): Int {
+        return input.lineSequence()
+            .map { splitInHalf(it) }
+            .map { (a, b) -> (a intersect b).single().priority() }
+            .sum()
+    }
+
+    override fun executePart2(input: String): Int {
+        return input.lineSequence()
+            .chunked(3)
+            .map { (a, b, c) -> (a.toSet() intersect b.toSet() intersect c.toSet()).single().priority() }
+            .sum()
     }
 }
